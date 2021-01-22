780e4f6d2227b42c15411c88fe1a92a761de947e
Guillaume Eveillard <eveillard.guillaume@gmail.com>
2021-01-22 01:49:49 +0100

    Use serde to serialize

--- tonic/src/codec/json.rs
+++ tonic/src/codec/json.rs
@@ -2,6 +2,9 @@ use super::{Codec, DecodeBuf, Decoder, Encoder};
 use crate::Status;
 use std::marker::PhantomData;
 use crate::codec::EncodeBuf;
+use bytes::{BufMut, Buf};
+use serde::{Serialize, Deserialize};
+use serde::de::DeserializeOwned;
 
 #[derive(Debug, Clone)]
 pub struct JsonCodec<T, U> {
@@ -16,8 +19,8 @@ impl<T, U> Default for JsonCodec<T, U> {
 
 impl<T, U> Codec for JsonCodec<T, U>
     where
-        T: /*Message +*/Sync + Send + 'static,
-        U: /*Message +*/Sync + Default + Send + 'static,
+        T: /*Message +*/Serialize + Sync + Send + 'static,
+        U: /*Message +*/DeserializeOwned + Sync + Default + Send + 'static,
 {
     type Encode = T;
     type Decode = U;
@@ -37,13 +40,20 @@ impl<T, U> Codec for JsonCodec<T, U>
 #[derive(Debug, Clone, Default)]
 pub struct JsonEncoder<T: Send + Sync>(PhantomData<T>);
 
-impl<T: Send + Sync/*: Message*/> Encoder for JsonEncoder<T> {
+impl<T: Send + Sync + Serialize/*: Message*/> Encoder for JsonEncoder<T> {
     type Item = T;
     type Error = Status;
 
     fn encode(&mut self, item: Self::Item, buf: &mut EncodeBuf<'_>) -> Result<(), Self::Error> {
-        // item.encode(buf)
-        //     .expect("Message only errors if not enough space");
+        // Convert the Point to a JSON string.
+        let serialized = serde_json::to_string(&item).unwrap();
+        buf.put(serialized.into_bytes().as_slice());
+
+        // Prints serialized = {"x":1,"y":2}
+        // println!("serialized = {}", serialized);
+
+        // Convert the JSON string back to a Point.
+        // let deserialized: Point = serde_json::from_str(&serialized).unwrap();
 
         Ok(())
     }
@@ -53,16 +63,19 @@ impl<T: Send + Sync/*: Message*/> Encoder for JsonEncoder<T> {
 #[derive(Debug, Clone, Default)]
 pub struct JsonDecoder<U>(PhantomData<U>);
 
-impl<U: /*Message + */Default> Decoder for JsonDecoder<U> {
+impl<U: /*Message + */Default + DeserializeOwned> Decoder for JsonDecoder<U> {
     type Item = U;
     type Error = Status;
 
     fn decode(&mut self, buf: &mut DecodeBuf<'_>) -> Result<Option<Self::Item>, Self::Error> {
+
+        let json = String::from_utf8_lossy(buf.chunk());
+        let deserialized: U = serde_json::from_str(&json.clone()).unwrap();
         // let item = Message::decode(buf)
         //     .map(Option::Some)
         //     .map_err(from_decode_error)?;
 
         // Ok(item)
-        Ok(Option::None)
+        Ok(Option::Some(deserialized))
     }
 }
